0a74ee9fe628168d4e9f8ccdd62773b5552c609b
luyvannda <philips.joe329@gmail.com>
2024-01-18 12:59:35 +0700

    initialize new project for shoes shop

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a tailwind project for Shoes Shop
+
+Based on the original designs from Udemy course
+
+https://tailwindcss-shoes-self.vercel.app/#
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Shoes Shop',
+  description: 'Project Shoes Shop is built with tailwind, react and nextjs',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
