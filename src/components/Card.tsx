da1c1033d82fea04be4385212d7328352dfb75cd
luyvannda <luyvannda@gmail.com>
2024-03-15 16:27:48 +0700

    style the shoes' Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,32 @@
-import { ShoeItem } from "@/constant";
-import Image from "next/image"
-import { FC } from "react";
-
+import { ShoeItem } from '@/constant';
+import Image from 'next/image';
+import { FC } from 'react';
 
 interface CardProps {
- item: ShoeItem;
+  item: ShoeItem;
 }
 
-const Card: FC<CardProps> = ({item}) => {
-    const {id, src, className, title, description, price} = item;
-    return <div>
-    <div>
-        <div>{title}</div>
-        <div>SHOP NOW +</div>
-    </div>
-        <Image
+const Card: FC<CardProps> = ({ item }) => {
+  const { id, src, color, title, description, price } = item;
+  return (
+    <div
+      className={`bg-[${color}] relative max-w-xl transform cursor-pointer transition hover:scale-105`}
+    >
+      <div className="p-8">
+        <div className="text-2xl font-bold">{title}</div>
+        <div className="mt-10 font-semibold underline underline-offset-4">
+          SHOP NOW +
+        </div>
+      </div>
+      <Image
         src={src}
         alt="Shoe Image"
-        width={300}
-        height={300}
-        ></Image>
+        width={180}
+        height={260}
+        className="absolute left-[40%] top-5 h-40 w-56"
+      ></Image>
     </div>
-}
+  );
+};
 
-export default Card
+export default Card;
