7565e07867d782e02dad3996ef59d70a13a333e1
luyvannda <luyvannda@gmail.com>
2024-04-04 15:38:37 +0700

    add cart item input and trash icon

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
-import { ShoeItem } from '@/constant';
+import { QTY, SIZES, ShoeItem } from '@/constant';
+import { CiTrash } from 'react-icons/ci';
+import Select from './Select';
 
 interface cartItemProps {
   item: ShoeItem;
@@ -7,20 +9,42 @@ interface cartItemProps {
 
 export default function CartItem({ item }: cartItemProps) {
   return (
-    <div className="flex cursor-pointer space-x-2 bg-gray-50 p-2 hover:bg-[#DAFFA2]">
-      <Image
-        src={item.src}
-        alt="shoes image"
-        width={120}
-        height={96}
-        className=""
-      ></Image>
+    <div className="cursor-pointer  space-y-2 bg-gray-50 p-2 hover:bg-[#DAFFA2]">
+      <div className="flex space-x-2">
+        {/* Image */}
+        <Image
+          src={item.src}
+          alt="shoes image"
+          width={120}
+          height={96}
+          className=""
+        ></Image>
 
-      <div className="space-y-2">
-        <div className="font-bold">{item.title}</div>
-        <div className="text-sm text-gray-400 ">{item.description}</div>
+        <div className="space-y-2">
+          {/* Title and Description */}
+          <div className="font-bold">{item.title}</div>
+          <div className="text-sm text-gray-400 ">{item.description}</div>
+        </div>
+
+        {/* Price */}
+        <div className="font-bold">{`$${item.price}`}</div>
       </div>
-      <div className="font-bold">{`$${item.price}`}</div>
+
+      <div className="flex justify-between pl-32">
+        <div className="flex space-x-6">
+          <div>
+            <div className="font-bold">SIZE</div>
+            <Select title="" options={SIZES} />
+          </div>
+          <div>
+            <div className="font-bold">QTY</div>
+            <Select title="" options={QTY} />
+          </div>
+        </div>
+        <button>
+          <CiTrash size={25} className="text-black" />
+        </button>
+      </div>
     </div>
   );
 }
